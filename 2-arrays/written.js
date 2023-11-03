9d55ba228db1f6b0e4826b577d1c26c5f6f34f4f
your name <nathanschroter@MacBook-Pro.local>
2023-11-03 15:18:19 -0700

    adding file and folder

---
+++ 2-arrays/written.js
@@ -0,0 +1,25 @@
+/**
+ * Arrays
+ * 
+ * always use camel case where needed
+ * 
+ * Make sure to write the question/instruction and then the answers below
+ * 
+ */
+
+/**
+ * Question 5 - concat two different arrays by using .concat() method
+ * 
+ *  
+ */
+
+
+
+
+
+
+/**
+ * Question 6 - create a 2 dimensions array that is 2-by-4. Grab some values out of this array to create a sentence using concatenation.
+ * 
+ *  
+ */
