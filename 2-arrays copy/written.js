874115053e03808888667bd8722678ecf5a56be3
nschroter <145606267+nschroter@users.noreply.github.com>
2023-11-04 15:40:30 -0700

    Add files via upload

---
+++ 2-arrays copy/written.js
@@ -0,0 +1,42 @@
+/**
+ * Arrays
+ * 
+ * always use camel case where needed
+ * 
+ * Make sure to write the question/instruction and then the answers below
+ * 
+ */
+
+/**
+ * Question 5 - concat two different arrays by using .concat() method
+ * 
+ *  
+ */
+/**
+ * initilise the variable fruits to the value of apple, banana and orange
+ * initilise the variable vegetables to the value of carrot, broccoli and spinach
+ * define the two arrays fruits and vegetables
+ * use the .concat() method to combine them into one aray called combinedArray
+ * console.log() the combinedArray
+ * comment out what is displayed
+ */
+var fruits =(apple, banana, orange)
+var vegetables=(carrot, broccoli, spinach)
+
+const fruits = ["apple", "banana", "orange"];
+const vegetables = ["carrot", "broccoli", "spinach"];
+
+
+const combinedArray = fruits.concat(vegetables);
+
+
+console.log(combinedArray);
+
+
+
+
+/**
+ * Question 6 - create a 2 dimensions array that is 2-by-4. Grab some values out of this array to create a sentence using concatenation.
+ * 
+ *  
+ */
